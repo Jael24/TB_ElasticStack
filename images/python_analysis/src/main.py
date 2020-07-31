f9aad11eda69045140a90f28739b558bf077d877
jael.dubey@heig-vd.ch <jael.dubey@heig-vd.ch>
2020-07-31 09:25:49 +0200

    Add python analysis

--- images/python_analysis/src/main.py
+++ images/python_analysis/src/main.py
@@ -1,19 +1,38 @@
 import time
 
-from elasticsearch import Elasticsearch
-import schedule
+import altair as alt
+import matplotlib
+import numpy as np
 import pandas as pd
+import schedule
+import statsmodels.api as sm
+import tad
+from elasticsearch import Elasticsearch
+from fbprophet import Prophet
+from matplotlib import pyplot as plt
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
+from pandas.plotting import register_matplotlib_converters
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.stattools import adfuller
+
+register_matplotlib_converters()
+
+matplotlib.style.use('ggplot')
 
 
 class AnalyseLogs:
+    """Class that allows to retrieve the logs from elasticsearch, and analyse them"""
+
     def __init__(self):
+        """Initialize the attribute"""
         self.logs = {"CPU Overload", "Nominal CPU utilization"}
         self.transactions = {0: {"CPU Overload": 0}}
         self.id_transaction = 0
+        self.cpu_list = []
 
     def get_last_events(self):
+        """Retrieve all the events which occurs in the last 15 seconds, and filter them for detecting CPU overload"""
         es = Elasticsearch()
         last_cpu_utilization = es.search(index="logstash-2020.07.01-000001", body={"query": {
             "bool": {
@@ -95,6 +114,7 @@ class AnalyseLogs:
         self.id_transaction += 1
 
     def get_all_events(self):
+        """Retrieve all the events, and filter them for detecting CPU overload"""
         es = Elasticsearch()
         all_cpu_utilization = es.search(index="logstash-2020.07.01-000001", body={"query": {
             "bool": {
@@ -150,7 +170,8 @@ class AnalyseLogs:
                         {
                             "range": {
                                 "@timestamp": {
-                                    "gte": all_cpu_utilization['hits']['hits'][i]['_source']['@timestamp'][:-1] + "||-20m",
+                                    "gte": all_cpu_utilization['hits']['hits'][i]['_source']['@timestamp'][
+                                           :-1] + "||-20m",
                                     "lt": all_cpu_utilization['hits']['hits'][i]['_source']['@timestamp'][:-1]
                                 }
                             }
@@ -176,7 +197,39 @@ class AnalyseLogs:
 
                 self.transactions[i][j] = 1 if log_is_present else 0
 
+    def get_all_cpu_pct(self):
+        """Retrieve all the logs which informs about cpu utilization"""
+        es = Elasticsearch()
+        cpu_utilization = es.search(index="logstash-2020.07.01-000001", body={"query": {
+            "bool": {
+                "must": [
+                    {
+                        "match": {
+                            "agent.type": "metricbeat"
+                        }
+                    },
+                    {
+                        "match": {
+                            "service.type": "system"
+                        }
+                    },
+                    {
+                        "match": {
+                            "event.dataset": "system.cpu"
+                        }
+                    }
+                ]
+            }
+        }
+        }, size=10000)
+
+        for i in range(cpu_utilization['hits']['total']['value']):
+            self.cpu_list.append((cpu_utilization['hits']['hits'][i]['_source']['@timestamp'][:-5],
+                                  cpu_utilization['hits']['hits'][i]['_source']['system']['cpu']['total']['pct']))
+
     def market_basket_analysis(self):
+        """Compute a market basket analysis on the logs stored in the class.
+           Inspired by https://pbpython.com/market-basket-analysis.html"""
         list_articles = list(self.logs)
 
         list_occ = []
@@ -189,35 +242,180 @@ class AnalyseLogs:
 
         df = pd.DataFrame(list_occ, columns=list_articles)
 
-        # Show DataFrame
-        # with pd.option_context('display.max_rows', None, 'display.max_columns',
-        #                        None):  # more options can be specified also
-        #     print("dataframe : ", df)
-
         frequent_itemsets = apriori(df, min_support=0.05, use_colnames=True)
 
         # Create the rules
         rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
 
-        print(rules[rules['consequents'] == {'CPU Overload'}]['consequents'])
+        # print(rules[rules['consequents'] == {'CPU Overload'}]['consequents'])
+
+    # def get_stationarity(self, timeseries):
+    # """Get the stationarity of a timeseries. Copied from https://moncoachdata.com/blog/modele-arima-avec-python/"""
+    #
+    #     # Statistiques mobiles
+    #     rolling_mean = timeseries.rolling(window=12).mean()
+    #     rolling_std = timeseries.rolling(window=12).std()
+    #
+    #     # tracé statistiques mobiles
+    #     original = plt.plot(timeseries, color='blue', label='Origine')
+    #     mean = plt.plot(rolling_mean, color='red', label='Moyenne Mobile')
+    #     std = plt.plot(rolling_std, color='black', label='Ecart-type Mobile')
+    #     plt.legend(loc='best')
+    #     plt.title('Moyenne et écart-type Mobiles')
+    #     plt.show(block=False)
+    #
+    #     # Test Dickey–Fuller :
+    #     result = adfuller(timeseries['Passengers'])
+    #     print('Statistiques ADF : {}'.format(result[0]))
+    #     print('p-value : {}'.format(result[1]))
+    #     print('Valeurs Critiques :')
+    #     for key, value in result[4].items():
+    #         print('\t{}: {}'.format(key, value))
+
+    def arima_forecast(self, csv_file, start_plot, end_plot, arima_parameters):
+        """Compute a forecasting operation with an ARIMA algorithm on a timeseries read in a CSV file
+        Inspired from https://moncoachdata.com/blog/modele-arima-avec-python/"""
+        df = pd.read_csv(csv_file, parse_dates=['Date'], index_col=['Date'])
+
+        model = ARIMA(df, order=arima_parameters)
+        results = model.fit(disp=-1)
+        fig = results.plot_predict(start_plot, end_plot)
+
+    def dparserfunc(self, date):
+        """Parse a date and create a datetime from a string"""
+        return pd.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+
+    # def twitter_anomaly_detection(self):
+    # """Test using the tad library from https://github.com/Marcnuth/AnomalyDetection but doesn't work"""
+    #     # first run the models
+    #     twitter_example_data = pd.read_csv('test_data_cpu.csv', index_col='timestamp',
+    #                                        parse_dates=True, squeeze=True,
+    #                                        date_parser=self.dparserfunc)
+    #     results = tad.anomaly_detect_ts(twitter_example_data, max_anoms=0.05, alpha=0.05, direction='both')
+    #
+    #     # # format the twitter data nicely
+    #     # twitter_example_data['timestamp'] = pd.to_datetime(twitter_example_data['timestamp'])
+    #     # twitter_example_data.set_index('timestamp', drop=True)
+    #     values = results['anoms'].get_values()
+    #
+    #     print(values)
+    #     # make a nice plot
+    #     f, ax = plt.subplots(2, 1, sharex=True)
+    #     # ax[0].plot(twitter_example_data['timestamp'], twitter_example_data['count'], 'b')
+    #     ax[0].plot(results['anoms'].index, results['anoms']['anoms'], 'ro')
+    #     ax[0].set_title('Detected Anomalies')
+    #     ax[1].set_xlabel('Time Stamp')
+    #     ax[0].set_ylabel('Count')
+    #     ax[1].plot(results['anoms'].index, results['anoms']['anoms'], 'b')
+    #     ax[1].set_ylabel('Anomaly Magnitude')
+    #     plt.show()
+
+    def plot_anomaly(self, df, timeseries):
+        """ Plot a graph from a timeseries with anomaly. The timeseries should be create by Prophet
+        Copied from https://towardsdatascience.com/anomaly-detection-time-series-4c661f6f165f"""
+        actual_vals = timeseries.actuals.values
+        actual_log = np.log10(actual_vals)
+
+        train, test = actual_vals[0:-70], actual_vals[-70:]
+
+        train_log, test_log = np.log10(train), np.log10(test)
+
+        my_order = (1, 1, 1)
+        my_seasonal_order = (0, 1, 1, 7)
+
+        history = [x for x in train_log]
+        predictions = list()
+        predict_log = list()
+        for t in range(len(test_log)):
+            model = sm.tsa.SARIMAX(history, order=my_order, seasonal_order=my_seasonal_order,
+                                   enforce_stationarity=False, enforce_invertibility=False)
+            model_fit = model.fit(disp=0)
+            output = model_fit.forecast()
+            predict_log.append(output[0])
+            yhat = 10 ** output[0]
+            predictions.append(yhat)
+            obs = test_log[t]
+            history.append(obs)
+
+    def fit_predict_model(self, dataframe, interval_width=0.85, changepoint_range=0.95):
+        """Create the model used by prophet to detect anomalies
+           Inspired by https://towardsdatascience.com/anomaly-detection-time-series-4c661f6f165f"""
+        m = Prophet(daily_seasonality=False, yearly_seasonality=False, weekly_seasonality=False,
+                    interval_width=interval_width,
+                    changepoint_range=changepoint_range)
+        m = m.fit(dataframe)
+        forecast = m.predict(dataframe)
+        forecast['fact'] = dataframe['y'].reset_index(drop=True)
+        return forecast
+
+    def detect_anomalies(self, forecast):
+        """Detect anomalies in the forecast realized by Prophet
+           Copied from https://towardsdatascience.com/anomaly-detection-time-series-4c661f6f165f"""
+        forecasted = forecast[['ds', 'trend', 'yhat', 'yhat_lower', 'yhat_upper', 'fact']].copy()
+
+        forecasted['anomaly'] = 0
+        forecasted.loc[forecasted['fact'] > forecasted['yhat_upper'], 'anomaly'] = 1
+        forecasted.loc[forecasted['fact'] < forecasted['yhat_lower'], 'anomaly'] = -1
+
+        # anomaly importances
+        forecasted['importance'] = 0
+        forecasted.loc[forecasted['anomaly'] == 1, 'importance'] = \
+            (forecasted['fact'] - forecasted['yhat_upper']) / forecast['fact']
+        forecasted.loc[forecasted['anomaly'] == -1, 'importance'] = \
+            (forecasted['yhat_lower'] - forecasted['fact']) / forecast['fact']
+
+        return forecasted
+
+    def plot_anomalies(self, forecasted):
+        """Show plot with anomalies
+           Inspired from https://towardsdatascience.com/anomaly-detection-time-series-4c661f6f165f"""
+        interval = alt.Chart(forecasted).mark_area(interpolate="basis", color='#adadad').encode(
+            x=alt.X('ds:T', title='Time'),
+            y='yhat_upper',
+            y2='yhat_lower',
+            tooltip=['ds', 'fact', 'yhat_lower', 'yhat_upper']
+        ).interactive().properties(
+            title='Anomaly Detection'
+        )
+
+        fact = alt.Chart(forecasted[forecasted.anomaly == 0]).mark_circle(size=15, opacity=0.7, color='Black').encode(
+            x='ds:T',
+            y=alt.Y('fact', title='CPU Utilization [%]'),
+            tooltip=['ds', 'fact', 'yhat_lower', 'yhat_upper']
+        ).interactive()
+
+        anomalies = alt.Chart(forecasted[forecasted.anomaly != 0]).mark_circle(size=30, color='Red').encode(
+            x='ds:T',
+            y=alt.Y('fact', title='CPU Utilization [%]'),
+            tooltip=['ds', 'fact', 'yhat_lower', 'yhat_upper'],
+            size=alt.Size('importance', legend=None)
+        ).interactive()
+
+        return alt.layer(interval, fact, anomalies) \
+            .properties(width=870, height=450) \
+            .configure_title(fontSize=20)
 
 
-    def print_state(self):
-        print("logs (articles) : ", self.logs)
-
-        print("transactions : ", self.transactions)
-
-        print("id transaction : ", self.id_transaction)
-
-        print("-------------------------------------------------------------")
-
+alt.renderers.enable('altair_viewer')
 
 if __name__ == '__main__':
     a = AnalyseLogs()
 
-    a.get_all_events()
-    a.market_basket_analysis()
+    # Dataframe used by the Prophet library
+    df = pd.read_csv('test_data_cpu.csv', parse_dates=['ds'])
+    df_sorted = df.sort_values(by=['ds'])
+    df_sorted['y'] = df['y'].apply(lambda x: x * 100 / 4)
 
+    # Analyze the time series with Prophet model, and show the results
+    forecast = a.fit_predict_model(df_sorted)
+    forecasted = a.detect_anomalies(forecast)
+    fig1 = a.plot_anomalies(forecasted)
+    fig1.show()
+
+    # # Read the logs with CPU utilization information and forecast the values
+    # a.arima_forecast('airline_passengers.csv', 1, 241, (2,1,1))
+
+    # Useful for the real-time analysis
     # schedule.every(10).seconds.do(a.get_last_events)
     # schedule.every(10).seconds.do(a.print_state)
     # schedule.every(2).minutes.do(a.market_basket_analysis)
